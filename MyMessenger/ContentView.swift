a360c848719e51b80643bfa277fd2fb26f16be06
Mohammad <mohammad.nej@gmail.com>
2023-12-10 19:21:03 +0330

    Initial Commit

---
+++ MyMessenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMessenger
+//
+//  Created by Mohammad on 9/19/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMessenger/MyMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMessengerApp.swift
+//  MyMessenger
+//
+//  Created by Mohammad on 9/19/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct MyMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
